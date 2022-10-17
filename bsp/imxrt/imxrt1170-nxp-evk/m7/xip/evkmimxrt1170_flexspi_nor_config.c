a807e7b785532ad9a28af3ce66392ee4b6f91caf
StackYuan <yuanjyjyj@outlook.com>
2022-10-17 10:25:47 +0800

    [change] mimxrt1170 bsp dcd file and flexspi_nor_cfg file.

--- bsp/imxrt/imxrt1170-nxp-evk/m7/xip/evkmimxrt1170_flexspi_nor_config.c
+++ bsp/imxrt/imxrt1170-nxp-evk/m7/xip/evkmimxrt1170_flexspi_nor_config.c
@@ -22,32 +22,34 @@ __attribute__((section(".boot_hdr.conf"), used))
 #pragma location = ".boot_hdr.conf"
 #endif
 
-const flexspi_nor_config_t qspiflash_config = {
+const flexspi_nor_config_t hyperflash_config = {                    //MaaXBoard RT update
     .memConfig =
         {
-            .tag              = FLEXSPI_CFG_BLK_TAG,
-            .version          = FLEXSPI_CFG_BLK_VERSION,
-            .readSampleClkSrc = kFlexSPIReadSampleClk_LoopbackFromDqsPad,
-            .csHoldTime       = 3u,
-            .csSetupTime      = 3u,
+            .tag                = FLEXSPI_CFG_BLK_TAG,
+            .version            = FLEXSPI_CFG_BLK_VERSION,
+            .readSampleClkSrc   = kFlexSPIReadSampleClk_ExternalInputFromDqsPad,
+            .csHoldTime         = 3u,
+            .csSetupTime        = 3u,
+            .columnAddressWidth = 3u,
             // Enable DDR mode, Wordaddassable, Safe configuration, Differential clock
-            .controllerMiscOption = 0x10,
-            .deviceType           = kFlexSpiDeviceType_SerialNOR,
-            .sflashPadType        = kSerialFlash_4Pads,
-            .serialClkFreq        = kFlexSpiSerialClk_133MHz,
-            .sflashA1Size         = 16u * 1024u * 1024u,
+            .controllerMiscOption =
+                (1u << kFlexSpiMiscOffset_DdrModeEnable) | (1u << kFlexSpiMiscOffset_WordAddressableEnable) |
+                (1u << kFlexSpiMiscOffset_SafeConfigFreqEnable) | (1u << kFlexSpiMiscOffset_DiffClkEnable),
+            .sflashPadType = kSerialFlash_8Pads,
+            .serialClkFreq = kFlexSpiSerialClk_133MHz,
+            .sflashA1Size  = 32u * 1024u * 1024u,
+            .dataValidTime = {16u, 16u},
             .lookupTable =
                 {
                     // Read LUTs
-                    FLEXSPI_LUT_SEQ(CMD_SDR, FLEXSPI_1PAD, 0xEB, RADDR_SDR, FLEXSPI_4PAD, 0x18),
-                    FLEXSPI_LUT_SEQ(MODE8_SDR, FLEXSPI_4PAD, 0x00, DUMMY_SDR, FLEXSPI_4PAD, 0x04),
-                    FLEXSPI_LUT_SEQ(READ_SDR, FLEXSPI_4PAD, 0x04, 0, 0, 0),
+                    FLEXSPI_LUT_SEQ(CMD_DDR, FLEXSPI_8PAD, 0xA0, RADDR_DDR, FLEXSPI_8PAD, 0x18),
+                    FLEXSPI_LUT_SEQ(CADDR_DDR, FLEXSPI_8PAD, 0x10, DUMMY_DDR, FLEXSPI_8PAD, 0x06),
+                    FLEXSPI_LUT_SEQ(READ_DDR, FLEXSPI_8PAD, 0x04, STOP, FLEXSPI_1PAD, 0x0),
                 },
         },
-    .pageSize           = 256u,
-    .sectorSize         = 4u * 1024u,
-    .ipcmdSerialClkFreq = 0x1,
+    .pageSize           = 512u,
+    .sectorSize         = 256u * 1024u,
     .blockSize          = 256u * 1024u,
-    .isUniformBlockSize = false,
+    .isUniformBlockSize = true,
 };
 #endif /* XIP_BOOT_HEADER_ENABLE */
